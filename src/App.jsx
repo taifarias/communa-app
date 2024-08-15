57e36010e0f0c040ac24498319a735aa566f8f22
Tainá <141168586+taifarias@users.noreply.github.com>
2024-08-15 19:00:43 -0300

    uniformizei o idioma
    tinha coisa em portugues e ingle, coloquei tudo pra ingles logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className='App'>
-      
-      
-     
-      
+    <div className='App'>    
       
       <Outlet />
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,31 +8,34 @@ const Login = () => {
             
             <section id="container" className="flex w-3/5 h-4/5 rounded-md ">        
                 <div id="cadastro" className="bg-white h-full w-1/2 rounded-l-md flex items-center flex-col pt-10">
-                <div className="pb-10">
-                    <img src={LogoImg} alt="logo-communa" className="w-36" />
+                <div className="pb-6 flex flex-col items-center">
+                    <img src={LogoImg} alt="logo-communa" className="w-28" />
                     <h1 className="font-semibold text-2xl">
-                        Comece agora na <span className="text-center text-purple-400 font-bold">Communa</span>
+                        Start now at <span className="text-center text-purple-400 font-bold">Communa</span>
                     </h1>
                 </div>
                 
                 <div className="">
-                        <p className="flex justify-start items-start pb-3 ">
-                        Por favor, faça o seu cadastro</p>
+                    <p className="flex justify-start items-start pb-3 text-purple-800">
+                        Create you account now</p>
 
-                        <div id="inputs" className="flex flex-col ">
+                    <div id="inputs" className="flex flex-col ">
 
-                            <input type="text" className="border border-gray-400 w-80 p-1 px-4 rounded-md mb-3 lg:w-" placeholder="Username" />
-                            <input type="text" className="border border-gray-400 w-80 p-1 px-4 rounded-md mb-3" placeholder="E-mail" />
-                            <input type="text" className="border border-gray-400 w-80 p-1 px-4 rounded-md mb-3" placeholder="Senha" />
-                            <input type="text" className="border border-gray-400 w-80 p-1 px-4 rounded-md mb-3" placeholder="Confirme sua senha" />
+                            <input type="text" className="border border-gray-400 w-72 p-1 px-4 rounded-md mb-3 lg:w-" placeholder="Username" />
+                            <input type="text" className="border border-gray-400 w-72 p-1 px-4 rounded-md mb-3" placeholder="E-mail" />
+                            <input type="text" className="border border-gray-400 w-72 p-1 px-4 rounded-md mb-3" placeholder="Password" />
+                            <input type="text" className="border border-gray-400 w-72 p-1 px-4 rounded-md mb-3" placeholder="Confirm password" />
                             
-                        </div>
+                    </div>
                     
-                <button className="bg-purple-400 border text-white py-2 px-4 rounded-lg ">
-                    Cadastrar e seguir
-                </button>
+                    <div className="flex justify-end">
+                            <button className="bg-purple-400 border text-white py-2 px-4 rounded-lg">
+                            Sing in
+                            </button>
+                    </div>
+                   
             
-            </div>
+                </div>
             
                 
 
@@ -44,12 +47,12 @@ const Login = () => {
             <div id="sidebar" className="bg-gradient-to-br from-violet-400 to-pink-200 h-full w-1/2 rounded-r-md flex justify-center items-center flex-col" >
 
             <h1 className="font-bold text-white text-xl pb-2 xl:text-lg lg:text-center">
-                Mais do que uma Rede. Uma Comunidade.
+                Mora than a network. A Community.
             </h1> 
             
 
-                <p className="py-6 px-20 text-center text-white xl:px-8 lg:px-4">
-                    Conecte-se. Compartilhe suas histórias, explore o desconhecido e descubra o poder da comunidade. <strong>Communa</strong>, onde o mundo está ao seu alcance.</p>
+                <p className="py-6 px-28 text-center text-white xl:px-8 lg:px-4">
+                    Conect. Share your stories, explore the unknow and discover the power of community. <strong>Communa</strong>, the world at your fingertips.</p>
 
 
             </div>
