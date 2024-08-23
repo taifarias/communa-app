6eb55c390c8aa38e1cef98e3ec8e5466ea8d5d97
Tainá <141168586+taifarias@users.noreply.github.com>
2024-08-23 18:26:43 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
- 
-})
+  base: '/communa-app/', // Substitua 'repo-name' pelo nome do seu repositório
+  
+  build: {
+    outDir: 'dist', // Pasta onde o build será colocado
+    rollupOptions: {
+      input: 'index.html', // Arquivo de entrada
+    },
+  },
+});
