fb15fe3a41eceaf595a4b1b43f5c24fd5b6676e4
Tainá <141168586+taifarias@users.noreply.github.com>
2024-08-23 18:42:05 -0300

    ed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/communa-app/', // Substitua 'repo-name' pelo nome do seu repositório
+  base: './', // Substitua 'repo-name' pelo nome do seu repositório
   
   build: {
     outDir: 'dist', // Pasta onde o build será colocado
